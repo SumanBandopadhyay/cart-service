e6c84ddbd7e61f57907064b42d3004e3d3261849
Suman Bandopadhyay <sumanbando006@gmail.com>
2021-05-08 19:24:52 +0530

    Heroku config

--- src/test/java/com/helpserviceprovider/cartservice/service/CartServiceTest.java
+++ src/test/java/com/helpserviceprovider/cartservice/service/CartServiceTest.java
@@ -1,5 +1,7 @@
 package com.helpserviceprovider.cartservice.service;
 
+import com.helpserviceprovider.cartservice.exception.CartDoesNotExistsException;
+import com.helpserviceprovider.cartservice.exception.CustomerIdEmptyException;
 import com.helpserviceprovider.cartservice.model.Cart;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,31 +13,40 @@ import org.springframework.data.redis.core.ReactiveValueOperations;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CartServiceTest {
 
   @Mock private ReactiveRedisTemplate<String, Cart> cartRedisTemplate;
 
-  @Mock private ReactiveValueOperations<String, Cart> cartValueOperations;
-
   @InjectMocks
   private CartService cartService;
 
   @Test
   void when_get_cart_exists_for_customer() {
-    when(cartRedisTemplate.hasKey(anyString())).thenReturn(Mono.just(true));
+    ReactiveValueOperations<String, Cart> cartValueOperations = mock(ReactiveValueOperations.class);
     when(cartRedisTemplate.opsForValue()).thenReturn(cartValueOperations);
+    Cart cart = Cart.builder().customerId("customerId").build();
     when(cartValueOperations.get(anyString()))
-            .thenReturn(Mono.just(new Cart()));
-//            .thenReturn(any(Mono.class));
-    StepVerifier.create(cartService.getCartByCustomerId(anyString())).verifyComplete();
+            .thenReturn(Mono.just(cart));
+    Mono<Cart> result = cartService.getCartByCustomerId("customerId");
+    StepVerifier.create(result).expectNext(cart).verifyComplete();
   }
 
-//  @Test
-//  void when_create_cart() {
-//
-//  }
+  @Test
+  void when_get_cart_does_not_exists_for_customer() {
+    ReactiveValueOperations<String, Cart> cartValueOperations = mock(ReactiveValueOperations.class);
+    when(cartRedisTemplate.opsForValue()).thenReturn(cartValueOperations);
+    when(cartValueOperations.get(anyString()))
+            .thenReturn(Mono.empty());
+    Mono<Cart> result = cartService.getCartByCustomerId("");
+    StepVerifier.create(result).expectError(CartDoesNotExistsException.class).verify();
+  }
+
+  @Test
+  void when_get_cart_does_not_provide_customer_id() {
+    Mono<Cart> result = cartService.getCartByCustomerId(null);
+    StepVerifier.create(result).expectError(CustomerIdEmptyException.class).verify();
+  }
 }
